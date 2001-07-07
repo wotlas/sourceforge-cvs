9c35874fc0d3d6f1f344f2671b0e0de3a29b277b
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2001-07-07 09:40:14 +0000

    windows script to build javadoc

---
+++ wotlas/docs/build/build-javadoc.bat
@@ -0,0 +1,6 @@
+REM To build the wotlas javadoc
+REM Do not modify this file, Edit the file named "package"
+REM in this directory
+
+javadoc -author -sourcepath ../../src/ -d ../apis -group "Server" "wotlas.server*" -group "Client" "wotlas.client*" -group "Common" "wotlas.common*" -group "Libraries" "wotlas.libs*" -group "Utilities" "wotlas.utils*" @packages
+

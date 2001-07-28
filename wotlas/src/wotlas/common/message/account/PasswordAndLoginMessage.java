0f693921d2e722758d9aa7d237c93d9aa9c07754
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2001-07-28 19:38:28 +0000

    account messages

---
+++ wotlas/src/wotlas/common/message/account/PasswordAndLoginMessage.java
@@ -0,0 +1,97 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+ 
+package wotlas.common.message.account;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import wotlas.libs.net.NetMessage;
+import wotlas.common.message.MessageRegistry;
+
+
+/** 
+ * To set the account's password & login. (Message Sent by Client)
+ *
+ * @author Aldiss
+ */
+
+public class PasswordAndLoginMessage extends NetMessage
+{
+ /*------------------------------------------------------------------------------------*/
+
+  /** Login
+   */
+      protected String login;
+
+  /** Password
+   */
+      protected String password;
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor. Just initializes the message category and type.
+   */
+     public PasswordAndLoginMessage() {
+          super( MessageRegistry.ACCOUNT_CATEGORY, AccountMessageCategory.PASSWORD_AND_LOGIN_MSG );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** Constructor with the client's login & password.
+   *
+   * @param login login
+   * @param password password
+   */
+     public PasswordAndLoginMessage( String login, String password ) {
+         this();
+         this.login = login;
+         this.password = password;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we put your message data on the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param ostream data stream where to put your data (see java.io.DataOutputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void encode( DataOutputStream ostream ) throws IOException {
+         writeString( login, ostream );
+         writeString( password, ostream );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we retrieve our message data from the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param istream data stream where you retrieve your data (see java.io.DataInputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void decode( DataInputStream istream ) throws IOException {
+          login = readString( istream );
+          password = readString( istream );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+}
+
---
+++ wotlas/src/wotlas/common/message/account/WarningMessage.java
@@ -0,0 +1,91 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+ 
+package wotlas.common.message.account;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import wotlas.libs.net.NetMessage;
+import wotlas.common.message.MessageRegistry;
+
+
+/** 
+ * A warning message that should display to the client in a pop-up window.
+ * (Message Sent by Server)
+ *
+ * @author Aldiss
+ */
+
+public class WarningMessage extends NetMessage
+{
+ /*------------------------------------------------------------------------------------*/
+
+   // information
+      protected String info;
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor. Just initializes the message category and type.
+   */
+     public WarningMessage() {
+          super( MessageRegistry.ACCOUNT_CATEGORY, AccountMessageCategory.ACCOUNT_WARNING_MSG );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** Constructor with IDs.
+   *
+   * @param clientID client local ID
+   * @param serverID server ID who created the client account
+   */
+     public WarningMessage(String info) {
+         this();
+         this.info = info;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we put your message data on the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param ostream data stream where to put your data (see java.io.DataOutputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void encode( DataOutputStream ostream ) throws IOException {
+         writeString( info, ostream );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we retrieve our message data from the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param istream data stream where you retrieve your data (see java.io.DataInputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void decode( DataInputStream istream ) throws IOException {
+          info = readString( istream );
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+}
+

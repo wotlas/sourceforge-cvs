2fccf9b43c7b6f272d01cd44037091a85047546f
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2001-07-28 19:49:10 +0000

    account server classes

---
+++ wotlas/src/wotlas/server/AccountBuilder.java
@@ -0,0 +1,205 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package wotlas.server;
+
+import wotlas.common.message.account.*;
+import wotlas.utils.Debug;
+import wotlas.libs.net.*;
+
+
+/** An AccountBuilder helps the creation of a GameAccount for a client. Here is
+ *  how it works :<p><br>
+ *
+ *  1 - The client connects to the AccountServer.<br>
+ *
+ *  2 - The AccountServer creates a new AccountBuilder and sets it as the client's
+ *      NetMessage context.<br>
+ *
+ *  3 - The client discusses with the AccountBuilder to build his GameAccount and
+ *      his PlayerImpl.<br>
+ *
+ *  4 - When the account is ready it is saved to disk via the PersistenceManager
+ *      and added to the current running game.<br>
+ *
+ *  5 - The client connection is then closed and the AccountBuilder handled to the
+ *      garbage collector. The client can now connect to the GameServer.
+ *
+ * @author Aldiss
+ * @see wotlas.server.AccountServer
+ */
+
+
+public class AccountBuilder implements NetConnectionListener
+{
+ /*------------------------------------------------------------------------------------*/
+
+    /** Account Empty State (not initialized)
+     */
+       static final public byte ACCOUNT_EMPTY_STATE = 0;
+
+    /** ADD HERE OTHER ACCOUNT STATE (player quizz, quizz done ... )
+     */
+
+    /** Account Ready State (account ready to be created)
+     */
+       static final public byte ACCOUNT_READY_STATE = 124;
+
+    /** Account Created State (account has been created)
+     */
+       static final public byte ACCOUNT_CREATED_STATE = 125;
+
+ /*------------------------------------------------------------------------------------*/
+
+   /** Actual State of the creation process. The possible values are described above.
+    */
+     private byte state;
+
+
+   /** The Game Account we are building
+    */
+     private GameAccount account;
+
+
+   /** The Player Data associated to this GameAccount
+    */
+     private PlayerImpl player;
+
+
+   /** Personality of our client.
+    */
+     private NetPersonality personality;
+ 
+
+   /** Our Account Server
+    */
+     private AccountServer accountServer;
+ 
+ /*------------------------------------------------------------------------------------*/
+
+   /** Constructor.
+    */
+      public AccountBuilder( AccountServer accountServer ) {
+           this.accountServer = accountServer;
+
+      	// the account is empty for now...
+           account = new GameAccount();
+           player = new PlayerImpl();
+           state = ACCOUNT_EMPTY_STATE;
+      }
+
+ /*------------------------------------------------------------------------------------*/
+
+   /** Method called when the connection with the client is established.
+    *
+    * @param personality 
+    */
+     public void connectionCreated( NetPersonality personality ) {
+         this.personality = personality;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+   /** Method called when the connection with the client is established.
+    *
+    * @param personality 
+    */
+     public void connectionClosed( NetPersonality personality ) {
+       // clean-up
+          personality = null;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+ 
+   /** Method called by NetMessages to set login and password.
+    */
+     public void setLoginAndPassword( String login, String password ) {
+     	if(state!=ACCOUNT_EMPTY_STATE) {
+           stateError();
+           return;
+        }
+
+     	account.setLogin(login);
+     	account.setPassword(password);                
+        state = ACCOUNT_READY_STATE;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+   /** Method called by NetMessages to create the account
+    */
+     public void createAccount() {
+     	if(state!=ACCOUNT_READY_STATE) {
+           stateError();
+           return;
+        }
+
+        PersistenceManager pm = PersistenceManager.getDefaultPersistenceManager();
+        AccountManager accountManager = DataManager.getDefaultDataManager().getAccountManager();
+
+     // final inits
+        account.setPlayer( player );
+        account.setLocalClientID( accountServer.getNewLocalClientID() );
+        account.setOriginalServerID( ServerManager.getDefaultServerManager().getServerConfig().getServerID() );
+        account.setLastConnectionTimeNow();
+
+     // account creation
+        if( accountManager.checkAccountName( account.getAccountName() ) ) {
+              personality.queueMessage( new AccountCreationFailedMessage( "Account already exists. Please change your login." ) );
+              return;
+        }
+
+        if( pm.createAccount( account ) ) {
+           // we add the account to the AccountManager & the player to the world...
+              accountManager.addAccount( account );
+              account.getPlayer().init();
+              
+              DataManager.getDefaultDataManager().getWorldManager().addNewPlayer( account.getPlayer() );
+              Debug.signal( Debug.NOTICE, this, "Added new client account to the game." );
+
+           // we send a Success Message
+              personality.queueMessage( new AccountCreatedMessage(account.getLocalClientID(),
+                                                account.getOriginalServerID() ) );
+
+           // And close the connection
+              personality.closeConnection();
+        }
+        else {
+           // Account not created for some reason
+           // we announce the bad news to the client
+           // but we don't close the connection...
+              personality.queueMessage( new AccountCreationFailedMessage( "internal failure" ) );
+        }
+
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+ 
+   /** A small method to report a state error and cancel the account creation
+    */
+     public void stateError() {
+        state=ACCOUNT_EMPTY_STATE;
+        Debug.signal( Debug.ERROR, this, "Bad State Detected During Account Creation" );
+        personality.closeConnection();
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+}
+
---
+++ wotlas/src/wotlas/server/AccountServer.java
@@ -0,0 +1,149 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package wotlas.server;
+
+import wotlas.libs.net.NetServer;
+import wotlas.libs.net.NetPersonality;
+
+import wotlas.utils.Debug;
+import wotlas.utils.FileTools;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/** Wotlas Account Server. Its role is to wait clients and connect them to a new
+ *  AccountBuilder. An AccountBuilder will help the client to create a GameAccount.<p>
+ *
+ *  For more information on how it works see {@see AccountBuilder AccountBuilder }.
+ *
+ * @author Aldiss
+ * @see wotlas.server.AccountBuilder
+ */
+
+public class AccountServer extends NetServer
+{
+ /*------------------------------------------------------------------------------------*/
+
+   /** Static Link to Account Server Config File.
+    */
+    public final static String ACCOUNT_CONFIG = "../src/config/account-server.cfg";
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Client Counter
+   */
+      private int clientCounter;      
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor (see wotlas.libs.net.NetServer for details)
+   *
+   *  @param host the host interface to bind to. Example: wotlas.tower.org
+   *  @param server_port port on which the server listens to clients.
+   *  @param msg_packages a list of packages where we can find NetMsgBehaviour Classes.
+   *  @param nbMaxSockets maximum number of sockets that can be opened on this server
+   */
+    public AccountServer( String host, int port, String packages[], int nbMaxSockets ) {
+       super( host, port, packages );
+       setMaximumOpenedSockets( nbMaxSockets );
+     
+     
+     // we load the clientCounter from the ACCOUNT_CONFIG
+        Properties props = FileTools.loadPropertiesFile( ACCOUNT_CONFIG );
+
+        if(props==null) {
+           // file not found, we create one...
+              Debug.signal( Debug.WARNING, this, "Could not find file: "+ACCOUNT_CONFIG
+                            +"\n   Creating a new account-server.cfg file..." );
+
+              props = new Properties();
+              props.setProperty( "clientCounter", "0" );
+              clientCounter=0;
+              
+              if( !FileTools.savePropertiesFile( props, ACCOUNT_CONFIG, "Do not remove or modify this file !") ){
+                  Debug.signal( Debug.FAILURE, this,"Cannot create or get account-server.cfg file!");
+                  System.exit(1);
+              }
+        }
+        else {
+             try{
+                clientCounter = Integer.parseInt( props.getProperty( "clientCounter" ) )+1;
+                Debug.signal( Debug.NOTICE, null, "AccountServer Client Counter set to "+clientCounter+"." );
+             }
+             catch( Exception e ){
+                  Debug.signal( Debug.FAILURE, this,"Bad account-server.cfg clientCounter property!");
+                  System.exit(1);
+             }
+        }
+    }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+   /** This method is called automatically when a new client establishes a connection
+    *  with this server ( the client sends a ClientRegisterMessage ).
+    *
+    * @param personality a previously created personality for this connection.
+    * @param key a string given by the client to identify itself. The key should be
+    *        equal to "AccountServerPlease!".
+    */
+    public void accessControl( NetPersonality personality, String key )
+    {
+       // The key is there to prevent wrong connections
+
+          if( key.equals("AccountServerPlease!") )
+          {
+            // ok, let's create an AccountBuilder for this future client.
+               AccountBuilder accountBuilder = new AccountBuilder(this);
+
+            // we set his message context to his player...
+               personality.setContext( accountBuilder );
+               personality.setConnectionListener( accountBuilder );
+
+            // welcome on board...
+               acceptClient( personality );
+               Debug.signal( Debug.NOTICE, this, "A new client is building a GameAccount...");
+          }
+          else {
+               Debug.signal( Debug.NOTICE, this, "A client tried to enter the AccountServer with a wrong key :"+key);
+               refuseClient( personality, "Wrong key for this server :"+key );
+          }
+    }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+    synchronized public int getNewLocalClientID() {
+        clientCounter++;
+
+      // save this state in config/accountServer.cfg
+         Properties props = new Properties();
+         props.setProperty( "clientCounter", ""+clientCounter );
+              
+         if( !FileTools.savePropertiesFile( props, ACCOUNT_CONFIG, "Do not remove or modify this file !") ){
+             Debug.signal( Debug.CRITICAL, this,"Cannot save clientCounter (="
+                           +clientCounter+") to account-server.cfg file!");
+         }
+
+      // we return the new value
+         return clientCounter;
+    }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+}

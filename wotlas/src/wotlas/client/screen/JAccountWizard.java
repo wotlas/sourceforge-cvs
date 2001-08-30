125e980f205f908aec6e534c6fd32eca14390dee
Pierre Nguyen <petrus0@users.sourceforge.net>
2001-08-30 22:13:22 +0000

    wizard to create accounts

---
+++ wotlas/src/wotlas/client/screen/JAccountWizard.java
@@ -0,0 +1,353 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package wotlas.client.screen;
+
+import wotlas.client.DataManager;
+import wotlas.client.gui.*;
+
+import wotlas.common.message.account.*;
+import wotlas.common.message.description.*;
+
+import wotlas.libs.net.NetPersonality;
+
+import javax.swing.JRadioButton;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/** A wizard to create an account
+ *
+ * @author Petrus
+ * @see wotlas.client.gui.JWizard
+ */
+public class JAccountWizard extends JWizard
+{
+
+ /*------------------------------------------------------------------------------------*/
+
+  private NetPersonality personality;
+
+  /** Constructor
+   */
+  public JAccountWizard(NetPersonality personality) {
+    super("Account creation", 400, 400);
+    setContext(personality);
+    this.personality = personality;
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Init the different steps of the wizard
+   */
+  public void init()
+  {
+
+    Step1 step1 = new Step1();
+    Step2 step2 = new Step2();
+    Step3 step3 = new Step3();
+
+    addStep(step1);
+    addStep(step2);
+    addStep(step3);
+
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Start the wizard
+   */
+  public void start() {
+    showStep(0);
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Called when the wizard is finished
+   */
+  protected void onFinished(Object context) {
+    System.out.println("finished");
+    personality.queueMessage( new AccountCreationMessage() );
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** step of wizard
+   */
+  class Step1 extends JWizardStep
+  {
+    private JLabel lbl_infos;
+    private JRadioButton bt_char1, bt_char2;
+    private ButtonGroup btGroup;
+    private JPanel mainPanel, formPanel;
+
+    /** called when the step is to be shown
+     */
+    public void onShow(Object context) {}
+
+    /** called when Next button is clicked
+     */
+    public void onNext(Object context) {
+      personality.queueMessage( new WotCharacterClassMessage(btGroup.getSelection().getActionCommand(), wotlas.common.character.AesSedai.AES_BLUE_AJAH) );
+    }
+
+    /** called when Previous button is clicked
+     */
+    public void onPrevious(Object context) {}
+
+    /** Constructor
+     */
+    public Step1()
+    {
+      super("Character");
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+      lbl_infos = new JLabel("Please, choose your chracter:");
+      lbl_infos.setAlignmentX(LEFT_ALIGNMENT);
+      add(lbl_infos);
+
+      mainPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      formPanel = new JPanel(new GridLayout(2,1,10,2));
+        bt_char1 = new JRadioButton("Aes Sedai");
+        bt_char1.setActionCommand("wotlas.common.character.AesSedai");
+        bt_char1.setSelected(true);
+
+        formPanel.add(bt_char1);
+        bt_char2 = new JRadioButton("GIJoe");
+        bt_char2.setActionCommand("GieJoe");
+        bt_char2.setEnabled(false);
+        formPanel.add(bt_char2);
+        // Group the radio buttons.
+        btGroup = new ButtonGroup();
+        btGroup.add(bt_char1);
+        btGroup.add(bt_char1);        
+      mainPanel.add(formPanel);
+
+      mainPanel.setAlignmentX(LEFT_ALIGNMENT);
+      add(mainPanel);
+
+      add(Box.createVerticalGlue());
+
+    }
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** step of wizard
+   */
+  class Step3 extends JWizardStep
+  {
+    private JLabel lbl_infos;
+    private JLabel lbl_nickname, lbl_fullname, lbl_email;
+    private JTextField tf_nickname, tf_fullname, tf_email;
+    private JPanel mainPanel, formPanel;
+
+    /** called when the step is to be shown
+     */
+    public void onShow(Object context) {}
+
+    /** called when Next button is clicked
+     */
+    public void onNext(Object context) {      
+      personality.queueMessage( new PlayerNamesMessage(tf_nickname.getText(), tf_fullname.getText()));
+    }
+
+    /** called when Previous button is clicked
+     */
+    public void onPrevious(Object context) {}
+
+    /** Consctructor
+     */
+    public Step3()
+    {
+      super("Informations (last step)");
+
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+      lbl_infos = new JLabel("Complete these fields:");
+      lbl_infos.setAlignmentX(LEFT_ALIGNMENT);
+      add(lbl_infos);
+
+      mainPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      formPanel = new JPanel(new GridLayout(3,2,10,2));
+        lbl_nickname = new JLabel("Nickname: ");
+        formPanel.add(lbl_nickname);
+        tf_nickname = new JTextField(10);
+        formPanel.add(tf_nickname);
+        lbl_fullname = new JLabel("Full name: ");
+        formPanel.add(lbl_fullname);
+        tf_fullname = new JTextField(10);
+        formPanel.add(tf_fullname);
+        lbl_email = new JLabel("Email: ");
+        formPanel.add(lbl_email);
+        tf_email = new JTextField(10);
+        formPanel.add(tf_email);
+
+      mainPanel.add(formPanel);
+
+      mainPanel.setAlignmentX(LEFT_ALIGNMENT);
+      add(mainPanel);
+
+      add(Box.createVerticalGlue());
+
+    }
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** step of wizard
+   */
+  class Step2 extends JWizardStep
+  {
+    private JLabel lbl_infos;
+    private JRadioButton bt_color0, bt_color1, bt_color2, bt_color3, bt_color4, bt_color5;
+    private ButtonGroup btGroup;
+    private ActionListener myListener;
+    private JPanel mainPanel, formPanel;
+    private byte hairColor;
+
+    /** called when the step is to be shown
+     */
+    public void onShow(Object context) {}
+
+    /** called when Next button is clicked
+     */
+    public void onNext(Object context) {      
+      personality.queueMessage( new VisualPropertiesMessage(hairColor) );
+    }
+
+    /** called when Previous button is clicked
+     */
+    public void onPrevious(Object context) {}
+
+    /** Consctructor
+     */
+    public Step2()
+    {
+      super("Description");
+
+      hairColor = wotlas.common.character.Human.GOLDEN_HAIR;
+
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+      lbl_infos = new JLabel("Choose your hair color:");
+      lbl_infos.setAlignmentX(LEFT_ALIGNMENT);
+      add(lbl_infos);
+
+      mainPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+      formPanel = new JPanel(new GridLayout(6,1,10,2));
+        bt_color0 = new JRadioButton("BALD");
+        bt_color0.setActionCommand("0");
+        bt_color0.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.BALD;
+          }
+        });
+        formPanel.add(bt_color0);
+        bt_color0.setEnabled(false);
+
+        bt_color1 = new JRadioButton("GOLDEN");
+        bt_color1.setActionCommand("1");
+        bt_color1.setSelected(true);
+        bt_color1.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.GOLDEN_HAIR;
+          }
+        });
+        formPanel.add(bt_color1);
+        bt_color1.setSelected(true);
+
+        bt_color2 = new JRadioButton("BROWN");
+        bt_color2.setActionCommand("2");
+        bt_color2.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.BROWN_HAIR;
+          }
+        });
+        formPanel.add(bt_color2);
+        bt_color2.setEnabled(false);
+
+        bt_color3 = new JRadioButton("BLACK");
+        bt_color3.setActionCommand("3");
+        bt_color3.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.BLACK_HAIR;
+          }
+        });
+        formPanel.add(bt_color3);
+        bt_color3.setEnabled(false);
+
+        bt_color4 = new JRadioButton("GREY");
+        bt_color4.setActionCommand("4");
+        bt_color4.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.GREY_HAIR;
+          }
+        });
+        formPanel.add(bt_color4);
+        bt_color4.setEnabled(false);
+
+        bt_color5 = new JRadioButton("WHITE");
+        bt_color5.setActionCommand("5");
+        bt_color5.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            hairColor = wotlas.common.character.Human.WHITE_HAIR;
+          }
+        });
+        formPanel.add(bt_color5);
+        bt_color5.setEnabled(false);
+
+        btGroup = new ButtonGroup();
+
+        btGroup.add(bt_color0);
+        btGroup.add(bt_color1);
+        btGroup.add(bt_color2);
+        btGroup.add(bt_color3);
+        btGroup.add(bt_color4);
+        btGroup.add(bt_color5);
+      mainPanel.add(formPanel);
+
+      mainPanel.setAlignmentX(LEFT_ALIGNMENT);
+      add(mainPanel);
+
+      add(Box.createVerticalGlue());
+
+    }
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Main. TEST
+   *
+   * @param argv this default param is not used.
+   */
+  /*static public void main(String argv[])
+  {
+    JAccountWizard host = new JAccountWizard();
+    host.init();
+    host.start();
+  }*/
+
+ /*------------------------------------------------------------------------------------*/
+
+}

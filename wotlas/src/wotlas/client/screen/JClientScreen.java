1eb76089920815b0048c8160f4610ce05b0d83e8
Pierre Nguyen <petrus0@users.sourceforge.net>
2001-08-30 22:11:58 +0000

    properly close the log when window is closed

--- wotlas/src/wotlas/client/screen/JClientScreen.java
+++ wotlas/src/wotlas/client/screen/JClientScreen.java
@@ -27,6 +27,8 @@ import wotlas.libs.graphics2D.*;
 import wotlas.libs.graphics2D.drawable.*;
 import wotlas.libs.graphics2D.policy.*;
 
+import wotlas.utils.Debug;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -55,25 +57,25 @@ public class JClientScreen extends JFrame
 
   private final static int northHeight = 50;
   public final static int leftWidth = 600;
-  
+
   private final static int gameHeight = 400;
   public final static int mapHeight = 400;
   private final static int gameMinHeight = 200;
-  
+
   private final static int thumbHeight = 100;
-  
+
   private final static int playerHeight = 300;
-  
+
   private final static int chatMinHeight = 80;
 
  /*------------------------------------------------------------------------------------*/
-  
-  private JInfosPanel infosPanel;  
+
+  private JInfosPanel infosPanel;
   private JMapPanel mapPanel;
   private GraphicsDirector gDirector;
   private JChatPanel chatPanel;
   private JPreviewPanel previewPanel;
-  private JPlayerPanel playerPanel;  
+  private JPlayerPanel playerPanel;
   private JLogPanel logPanel;
 
  /*------------------------------------------------------------------------------------*/
@@ -82,31 +84,38 @@ public class JClientScreen extends JFrame
    */
   public JClientScreen() {
     super("Wotlas client");
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
   }
-  
+
   public JClientScreen(JInfosPanel infosPanel, GraphicsDirector gDirector, JChatPanel chatPanel,
                        JPreviewPanel previewPanel, JPlayerPanel playerPanel, JLogPanel logPanel) {
     super("Wotlas client");
-    this.infosPanel = infosPanel;    
+    this.infosPanel = infosPanel;
     this.gDirector = gDirector;
     this.chatPanel = chatPanel;
     this.previewPanel = previewPanel;
     this.playerPanel = playerPanel;
     this.logPanel = logPanel;
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    addWindowListener( new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        Debug.exit();
+      }
+    });
   }
-  
-  public JClientScreen(JInfosPanel infosPanel, JMapPanel mapPanel, JChatPanel chatPanel,
+
+  public JClientScreen(JInfosPanel infosPanel, JMapPanel myMapPanel, JChatPanel chatPanel,
                        JPreviewPanel previewPanel, JPlayerPanel playerPanel, JLogPanel logPanel) {
     super("Wotlas client");
     this.infosPanel = infosPanel;
-    this.mapPanel = mapPanel;    
+    this.mapPanel = myMapPanel;
     this.chatPanel = chatPanel;
     this.previewPanel = previewPanel;
     this.playerPanel = playerPanel;
     this.logPanel = logPanel;
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    addWindowListener( new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        mapPanel.exit();
+      }
+    });
   }
 
  /*------------------------------------------------------------------------------------*/
@@ -116,7 +125,7 @@ public class JClientScreen extends JFrame
   public void init()
   {
     // *** North panel ***
-    
+
     infosPanel.setPreferredSize(new Dimension(mainWidth, northHeight));
     infosPanel.setBackground(Color.red);
     getContentPane().add(infosPanel, BorderLayout.NORTH);
@@ -128,17 +137,17 @@ public class JClientScreen extends JFrame
       rightPanel.setBackground(Color.blue);
 
       // *** Preview Panel ***
-      
+
       previewPanel.setPreferredSize(new Dimension(mainWidth-leftWidth, thumbHeight));
       rightPanel.add(previewPanel);
 
       // *** Player Panel ***
-      
+
       playerPanel.setPreferredSize(new Dimension(mainWidth-leftWidth, playerHeight));
       rightPanel.add(playerPanel);
 
       // *** Log Panel ***
-      
+
       logPanel.setPreferredSize(new Dimension(mainWidth-leftWidth, mainHeight-thumbHeight-playerHeight));
       rightPanel.add(logPanel);
 
@@ -147,11 +156,11 @@ public class JClientScreen extends JFrame
     // *** Left panel ***
 
       // *** Map panel ***
-      
+
       mapPanel.setMinimumSize(new Dimension(leftWidth, gameMinHeight));
       //gDirector.setMinimumSize(new Dimension(leftWidth, gameMinHeight));
 
-      // *** Chat Panel ***      
+      // *** Chat Panel ***
 
       JScrollPane myChatPanel = new JScrollPane(chatPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
       //myChatPanel.setMinimumSize(new Dimension(leftWidth, mainHeight-northHeight-gameHeight));
@@ -159,7 +168,7 @@ public class JClientScreen extends JFrame
 
       JSplitPane leftPanel = new JSplitPane(JSplitPane.VERTICAL_SPLIT, mapPanel, myChatPanel);
       //JSplitPane leftPanel = new JSplitPane(JSplitPane.VERTICAL_SPLIT, gDirector, myChatPanel);
-      
+
       leftPanel.setOneTouchExpandable(true);
       leftPanel.setDividerLocation(gameHeight);
 
@@ -170,85 +179,85 @@ public class JClientScreen extends JFrame
   }
 
  /*------------------------------------------------------------------------------------*/
- 
+
   /** To set
    */
   public void setInfosPanel(JInfosPanel infosPanel) {
     this.infosPanel = infosPanel;
   }
-  
+
   /** To get
    */
   public JInfosPanel getInfosPanel() {
     return infosPanel;
   }
-  
+
   /** To set
    */
   public void setPreviewPanel(JPreviewPanel previewPanel) {
     this.previewPanel = previewPanel;
   }
-  
+
   /** To get
    */
   public JPreviewPanel getPreviewPanel() {
     return previewPanel;
   }
-  
+
   /** To set
    */
   public void setChatPanel(JChatPanel chatPanel) {
     this.chatPanel = chatPanel;
   }
-  
+
   /** To get
    */
   public JChatPanel getChatPanel() {
     return chatPanel;
   }
-  
+
   /** To set
    */
   public void setPlayerPanel(JPlayerPanel playerPanel) {
     this.playerPanel = playerPanel;
   }
-  
+
   /** To get
    */
   public JPlayerPanel getPlayerPanel() {
     return playerPanel;
   }
-  
+
   /** To set
    */
   public void setMapPanel(JMapPanel mapPanel) {
     this.mapPanel = mapPanel;
   }
-  
+
   /** To get
    */
   public JMapPanel getMapPanel() {
     return mapPanel;
   }
-   
+
  /*------------------------------------------------------------------------------------*/
 
   /*public static void main(String arv[])
-  {    
+  {
     JInfosPanel infosPanel = new JInfosPanel();
     JMapPanel mapPanel = new JMapPanel();
     JChatPanel chatPanel = new JChatPanel();
     JPreviewPanel previewPanel = new JPreviewPanel();
     JPlayerPanel playerPanel = new JPlayerPanel();
     JLogPanel logPanel = new JLogPanel();
-    
+
     JClientScreen mFrame = new JClientScreen(infosPanel, mapPanel, chatPanel, previewPanel, playerPanel, logPanel);
-    
+
     mFrame.init();
     mFrame.pack();
     mFrame.show();
   }*/
-  
+
  /*------------------------------------------------------------------------------------*/
 
 }
--- wotlas/src/wotlas/client/screen/ScreenIntro.java
+++ wotlas/src/wotlas/client/screen/ScreenIntro.java
@@ -16,8 +16,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
- 
+
 package wotlas.client.screen;
+
+import wotlas.utils.Debug;
 
 import javax.swing.*;
 import java.awt.*;
@@ -31,113 +33,116 @@ import java.awt.event.*;
 public class ScreenIntro extends JFrame
 {
   private JPanel leftPanel;
-  
+
   private JPanel rightPanel;
-  
+
   private int leftWidth = 290;
-  
+
   private int rightWidth = 110;
-  
-  private int height = 300;  
-  
+
+  private int height = 300;
+
   final private ImageIcon backgroundImg = new ImageIcon("D:\\projects\\wotlas\\wotlas\\src\\wotlas\\client\\screen\\bg-400x300.jpg");
-  
+
  /*------------------------------------------------------------------------------------*/
-  
+
   /** Constructor
    */
   public ScreenIntro() {
-    super("Wotlas client");        
-    setDefaultCloseOperation(EXIT_ON_CLOSE);        
-           
-    //JOptionPane.showMessageDialog(this, "Welcome to WOTLAS Client v1.0 (08-2001)\n\nsee http://wotlas.sf.net for latest version", "WOTLAS-Client", JOptionPane.INFORMATION_MESSAGE);    
+    super("Wotlas client");
+    //setDefaultCloseOperation(EXIT_ON_CLOSE);
+    addWindowListener( new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        Debug.exit();
+      }
+    });
   }
-  
-  
+
+
  /*------------------------------------------------------------------------------------*/
-  
+
   /** Set the left JPanel of the interface
    *
    * @param leftPanel left panel to be added
-   */  
+   */
   public void setLeftPanel(JPanel leftPanel) {
     removeLeftPanel();
     leftPanel.setPreferredSize( new Dimension(leftWidth, height) );
     this.leftPanel = leftPanel;
-    this.leftPanel.setOpaque(false);
+    //this.leftPanel.setOpaque(false);
     getContentPane().add(this.leftPanel, BorderLayout.WEST);
   }
-  
+
   /** Set the right JPanel of the interface
    *
    * @param rightPanel right panel to be added
    */
   public void setRightPanel(JPanel rightPanel) {
-    removeRightPanel();   
+    removeRightPanel();
     rightPanel.setPreferredSize( new Dimension(rightWidth, height) );
     this.rightPanel = rightPanel;
-    this.rightPanel.setOpaque(false);
+    //this.rightPanel.setOpaque(false);
     getContentPane().add(this.rightPanel, BorderLayout.EAST);
   }
-  
+
   public void removeLeftPanel() {
     if (this.leftPanel != null) {
       remove(this.leftPanel);
       leftPanel = null;
     }
   }
-  
+
   public void removeRightPanel() {
     if (this.rightPanel != null) {
       remove(this.rightPanel);
       rightPanel = null;
     }
-  }  
-  
+  }
+
  /*------------------------------------------------------------------------------------*/
-  
+
   /** To get left panel width
    */
   public int getLeftWidth() {
     return leftWidth;
   }
-  
+
   /** To set left panel width
    */
   public void setLeftWidth(int width) {
     leftWidth = width;
   }
-  
+
   /** To get right panel width
    */
   public int getRightWidth() {
     return rightWidth;
   }
-  
+
   /** To set right panel width
    */
   public void setRightWidth(int width) {
     rightWidth = width;
-  }  
-  
+  }
+
  /*------------------------------------------------------------------------------------*/
-  
+
   /** Show the interface
    */
-  public void showScreen() {                         
-    JPanel middlePanel = new JPanel() {      
+  public void showScreen() {
+    JPanel middlePanel = new JPanel() {
       {setOpaque(false);}
-      public void paintComponent(Graphics g) {                
+      public void paintComponent(Graphics g) {
         ImageIcon img = new ImageIcon("bg-400x300.jpg");
         g.drawImage(img.getImage(), 0, 0, null);
         super.paintComponent(g);
       }
-    };   
+    };
     getContentPane().add(middlePanel, BorderLayout.NORTH);
     pack();
     show();
   }
-  
+
   /** Close the interface
    */
   public void closeScreen() {
@@ -148,30 +153,30 @@ public class ScreenIntro extends JFrame
       remove(rightPanel);
     }
     dispose();
-  }    
-  
- /*------------------------------------------------------------------------------------*/ 
- 
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
   public void paint(Graphics g) {
     //ImageIcon image = new ImageIcon("FIREFALL.gif");
     /*Dimension d = getSize();
     for( int x = 0; x < d.width; x += backgroundImg.getIconWidth() )
     for( int y = 0; y < d.height; y += backgroundImg.getIconHeight() )*/
-    
+
     g.drawImage( backgroundImg.getImage(), 100, 100, null, null );
     super.paint(g);
     // Now let the paint do its usual work
-    
+
   }
-  
+
   /*public void paintComponent (Graphics g) {
     g.drawImage(backgroundImg, 0, 0, null);
-    super.paintComponent(g);  
+    super.paintComponent(g);
   }*/
- /*------------------------------------------------------------------------------------*/ 
-  
-}  
-    
-    
-    
+ /*------------------------------------------------------------------------------------*/
+
+}
+
+
+
     

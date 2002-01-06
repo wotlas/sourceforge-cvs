b8a283444554a8bf49472f42107aaed0f1a6b547
Pierre Nguyen <petrus0@users.sourceforge.net>
2002-01-06 13:51:15 +0000

    add log file

---
+++ wotlas/src/wotlas/client/screen/JChatDisplay.java
@@ -0,0 +1,178 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package wotlas.client.screen;
+
+import wotlas.client.*;
+
+import wotlas.common.chat.ChatRoom;
+
+import wotlas.libs.log.*;
+
+import wotlas.utils.MyHTMLEditorKit;
+import wotlas.utils.Tools;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import java.io.*;
+import java.util.*;
+
+/** JEditorPane where messages are displayed...<br>
+ * Messages are logged in a HTML file
+ *
+ * @author Petrus
+ */
+
+public class JChatDisplay extends LogStream
+{
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Static Link to Chat Log File.
+   */
+  public final static String CHAT_LOG_PREFIX = "../log/";
+  public final static String CHAT_LOG_SUFFIX = ".htm";
+
+  /** max number of messages to display on screen at the same time
+   */
+  static final private int MAX_DISPLAYED_MESSAGES = 25;
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** messages number.
+   */
+  private int msg_number;
+
+  /** Where messages appear.
+   */
+  private JEditorPane messagesPane;
+
+  /** string buffer.
+   */
+  private String strBuffer;
+  
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor.
+   *
+   * @param chatRoom chatRoom associated to this JChatDisplay
+   */
+  JChatDisplay(ChatRoom chatRoom) throws FileNotFoundException {
+    super( CHAT_LOG_PREFIX + chatRoom.getPrimaryKey() + CHAT_LOG_SUFFIX, true, 60*1000 );
+    
+    msg_number = 0;
+    strBuffer = "";
+    
+    messagesPane = new JEditorPane();
+    messagesPane.setEditable(false);
+    
+    MyHTMLEditorKit kit = new MyHTMLEditorKit();
+    messagesPane.setEditorKit(kit);
+    
+    print("<font color='green'><i>Entering " + chatRoom.getName() + " chat room</i></font><br>\n");
+
+  }
+
+  /** To get the scroll pane
+   */
+  public JScrollPane getPanel() {
+    JScrollPane displayScroller = new JScrollPane(messagesPane);
+    return displayScroller;
+  }
+  
+ /*------------------------------------------------------------------------------------*/
+
+  /** To append some text
+   * 
+   * @param text string to append
+   */
+  synchronized public void appendText(String text) {
+    
+    if ( (text.toLowerCase().indexOf("<html")>-1)
+         || (text.toLowerCase().indexOf("</html")>-1)
+         || (text.toLowerCase().indexOf("<pre")>-1) ) {
+      return;
+    }      
+      
+    // Search for smileys
+    text = Tools.subString(text, "0:)",  "<img width=16 height=20 src='file:../base/gui/chat/angel.gif'>");      
+    text = Tools.subString(text, ":,(", "<img width=15 height=15 src='file:../base/gui/chat/cry.gif'>");
+    text = Tools.subString(text, ":o",  "<img width=15 height=15 src='file:../base/gui/chat/eek.gif'>");
+    text = Tools.subString(text, ":D",  "<img width=15 height=15 src='file:../base/gui/chat/laugh.gif'>");
+    text = Tools.subString(text, ":(",  "<img width=15 height=15 src='file:../base/gui/chat/mad.gif'>");
+    text = Tools.subString(text, ">0",  "<img width=15 height=15 src='file:../base/gui/chat/rant.gif'>");
+    text = Tools.subString(text, "|I",  "<img width=15 height=24 src='file:../base/gui/chat/sleep.gif'>");
+    text = Tools.subString(text, ":)",  "<img width=15 height=15 src='file:../base/gui/chat/smile.gif'>");
+    text = Tools.subString(text, ":-)",  "<img width=15 height=15 src='file:../base/gui/chat/smile.gif'>");
+    text = Tools.subString(text, ":|",  "<img width=15 height=15 src='file:../base/gui/chat/squint.gif'>");
+    text = Tools.subString(text, ";)",  "<img width=15 height=15 src='file:../base/gui/chat/wink.gif'>");
+
+    text = Tools.subString(text, "<|",  "<img width=15 height=15 src='file:../base/gui/chat/rolleyes.gif'>");
+    text = Tools.subString(text, ":/",  "<img width=15 height=22 src='file:../base/gui/chat/confused.gif'>");
+    text = Tools.subString(text, ">|",  "<img width=15 height=15 src='file:../base/gui/chat/shake.gif'>");
+    text = Tools.subString(text, ">)",  "<img width=15 height=15 src='file:../base/gui/chat/devil.gif'>");
+    text = Tools.subString(text, ">D",  "<img width=15 height=15 src='file:../base/gui/chat/evilgrin.gif'>");
+    text = Tools.subString(text, ">(",  "<img width=16 height=16 src='file:../base/gui/chat/madfire.gif'>");
+
+    text = Tools.subString(text, ";P",  "<img width=15 height=15 src='file:../base/gui/chat/flirt.gif'>");
+    text = Tools.subString(text, "8D",  "<img width=15 height=15 src='file:../base/gui/chat/horny.gif'>");
+    text = Tools.subString(text, ">#",  "<img width=15 height=15 src='file:../base/gui/chat/nono.gif'>");
+    text = Tools.subString(text, "|O",  "<img width=15 height=15 src='file:../base/gui/chat/yawn.gif'>");
+
+    text += "<br>";
+    println(text);   
+  }
+
+ /*------------------------------------------------------------------------------------*/
+  
+  /** To print some text to the screen
+   *
+   * @param text string to print
+   */  
+  protected void printedText( final String text ) {
+    
+    // too much messages displayed ?
+    msg_number++;
+
+    if (DataManager.SHOW_DEBUG)
+      System.out.println("msg_number = " + msg_number);
+
+    if ( msg_number>MAX_DISPLAYED_MESSAGES ) {
+      int pos = strBuffer.indexOf("\n");
+      strBuffer = strBuffer.substring(pos+1);
+      msg_number--;
+    }
+    
+    strBuffer += text + "\n";
+  	Runnable runnable = new Runnable() {
+      public void run() {
+        messagesPane.setText(strBuffer);
+        messagesPane.repaint();
+      }
+    };
+    SwingUtilities.invokeLater( runnable );
+    
+  }
+}
+
+  
--- wotlas/src/wotlas/client/screen/JChatRoom.java
+++ wotlas/src/wotlas/client/screen/JChatRoom.java
@@ -23,6 +23,8 @@ import wotlas.client.*;
 
 import wotlas.common.chat.ChatRoom;
 
+import wotlas.libs.log.*;
+
 import wotlas.utils.MyHTMLEditorKit;
 import wotlas.utils.Tools;
 
@@ -46,29 +48,13 @@ public class JChatRoom extends JPanel
 
  /*------------------------------------------------------------------------------------*/
 
-  /** max number of messages to display on screen at the same time
-   */
-  static final private int MAX_DISPLAYED_MESSAGES = 25;
-
- /*------------------------------------------------------------------------------------*/
-
-  /** messages number.
-   */
-  private int msg_number;
-
   /** ChatRoom display.
    */
   private JPanel chatTab;
-
-  /** Where messages appear.
+  
+  /** Panel where messages appear.
    */
-  private JEditorPane messagesPane;
-
-  /** Chat document.
-   */
-  private DefaultStyledDocument doc_chat;
-  private SimpleAttributeSet attribut;
-  private String strBuffer;
+  private JChatDisplay chatDisplay;
 
   /** List of ChatRoom players.
    */
@@ -78,7 +64,7 @@ public class JChatRoom extends JPanel
   /** Player list
    */
   private Hashtable players;
-
+  
  /*------------------------------------------------------------------------------------*/
 
   /** Constructor.<br>
@@ -93,65 +79,6 @@ public class JChatRoom extends JPanel
     setLayout(new BorderLayout());
     players = new Hashtable(2);
 
-    msg_number = 0;
-
-    // CENTER (JPanel where messages appear)
-    /*HTMLEditorKit kit = new HTMLEditorKit();
-   	HTMLDocument doc_chat = (HTMLDocument) kit.createDefaultDocument();
-    */
-//    doc_chat = new DefaultStyledDocument();
-
-//    messagesPane = new JTextPane(doc_chat); ALDISS
-    messagesPane = new JEditorPane();
-    messagesPane.setEditable(false);
-    /*
-    messagesPane.setContentType("text/html");
-    messagesPane.setEditable(false);
-    */
-    
-    MyHTMLEditorKit kit = new MyHTMLEditorKit();
-    messagesPane.setEditorKit(kit);
-    
-    /*Document doc = messagesPane.getDocument();
-    
-    strBuffer = "<font color='green'><i>new chat created</i> <img src='file:../base/gui/chat/cry.gif'></font>";
-    StringReader reader = new StringReader(strBuffer);
-    try {
-      kit.read(reader, doc, 0);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    System.out.println("doc.getLength() = " + doc.getLength());
-    try {
-      System.out.println("doc = " + doc.getText(0, doc.getLength()));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    
-    reader = new StringReader("<br>toto <i>est</i> beau!!<br>Eh <b>oui</b>");
-    try {
-      kit.read(reader, doc, doc.getLength());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    System.out.println("doc.getLength() = " + doc.getLength());
-    try {
-      System.out.println("doc = " + doc.getText(0, doc.getLength()));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }*/
-    
-    JScrollPane displayScroller = new JScrollPane(messagesPane);
-    
-    attribut = new SimpleAttributeSet();
-    StyleConstants.setFontSize(attribut,12);
-
-    /*
-    strBuffer = "<font color='green'><i>new chat created</i></font><br>\n";;
-    messagesPane.setText("<html><body>" + strBuffer + "</body></html>");
-    */
-
-    //messagesPane.setCaretPosition(messagesPane.getText().length());
 
     // EAST (List of ChatRoom players)
     playersListModel = new DefaultListModel();
@@ -166,14 +93,17 @@ public class JChatRoom extends JPanel
             ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
             ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-    //playersJList.setPreferredSize(new Dimension(100,0));
-    //playersJList.setMinimumSize(new Dimension(100,0));
-    //playersJList.setMaximumSize(new Dimension(100,0));
-    
-    add("Center", displayScroller);
-    add("East", listScroller);
+    try {
+      chatDisplay = new JChatDisplay(chatRoom);
+    } catch( java.io.FileNotFoundException e ) {
+      e.printStackTrace();
+      return;
+    }
 
-    strBuffer = "";
+    add("Center", chatDisplay.getPanel());
+
+    add("East", listScroller);
+    
     appendText("<font color='green'><i>new chat created</i></font>");
 
   }
@@ -263,105 +193,10 @@ public class JChatRoom extends JPanel
   }
 
  /*------------------------------------------------------------------------------------*/
-
-  synchronized public void appendText(String text) {
-    
-    if ( (text.toLowerCase().indexOf("<html")>-1)
-         || (text.toLowerCase().indexOf("</html")>-1)
-         || (text.toLowerCase().indexOf("<pre")>-1) ) {
-      return;
-    }
-    
-    // too much messages displayed ?
-    msg_number++;
-
-    /*StringBuffer buffer = messagesPane.getText();
-    System.out.println("buffer = " + buffer);
-    buffer = buffer + text + "<br>";
-    System.out.println("buffer after = " + buffer);
-    messagesPane.setText(buffer);*/
-
-    if (DataManager.SHOW_DEBUG)
-      System.out.println("msg_number = " + msg_number);
-    /*try {
-      System.out.println("dochat = " + doc_chat.getText(0,doc_chat.getLength()));
-      System.out.println("messagesPane.getText() = " + messagesPane.getText());
-    } catch (BadLocationException e) {
-        System.out.println("Chat Error:"+e.getMessage());
-    }*/
-
-    if ( msg_number>MAX_DISPLAYED_MESSAGES )
-      //try
-      {
-        /*int pos = doc_chat.getText(0,doc_chat.getLength()).indexOf("\n");
-        doc_chat.remove(0,pos+1);*/
-
-        int pos = strBuffer.indexOf("\n");
-        strBuffer = strBuffer.substring(pos+1);
-        msg_number--;
-	    /*} catch(BadLocationException e) {
-        System.out.println("Chat Error:"+e.getMessage());*/
-      }
-
-      // Search for commands
-
-      
-      
-      // Search for smileys
-      text = Tools.subString(text, "0:)",  "<img width=16 height=20 src='file:../base/gui/chat/angel.gif'>");      
-      text = Tools.subString(text, ":,(", "<img width=15 height=15 src='file:../base/gui/chat/cry.gif'>");
-      text = Tools.subString(text, ":o",  "<img width=15 height=15 src='file:../base/gui/chat/eek.gif'>");
-      text = Tools.subString(text, ":D",  "<img width=15 height=15 src='file:../base/gui/chat/laugh.gif'>");
-      text = Tools.subString(text, ":(",  "<img width=15 height=15 src='file:../base/gui/chat/mad.gif'>");
-      text = Tools.subString(text, ">0",  "<img width=15 height=15 src='file:../base/gui/chat/rant.gif'>");
-      text = Tools.subString(text, "|I",  "<img width=15 height=24 src='file:../base/gui/chat/sleep.gif'>");
-      text = Tools.subString(text, ":)",  "<img width=15 height=15 src='file:../base/gui/chat/smile.gif'>");
-      text = Tools.subString(text, ":-)",  "<img width=15 height=15 src='file:../base/gui/chat/smile.gif'>");
-      text = Tools.subString(text, ":|",  "<img width=15 height=15 src='file:../base/gui/chat/squint.gif'>");
-      text = Tools.subString(text, ";)",  "<img width=15 height=15 src='file:../base/gui/chat/wink.gif'>");
-
-      text = Tools.subString(text, "<|",  "<img width=15 height=15 src='file:../base/gui/chat/rolleyes.gif'>");
-      text = Tools.subString(text, ":/",  "<img width=15 height=22 src='file:../base/gui/chat/confused.gif'>");
-      text = Tools.subString(text, ">|",  "<img width=15 height=15 src='file:../base/gui/chat/shake.gif'>");
-      text = Tools.subString(text, ">)",  "<img width=15 height=15 src='file:../base/gui/chat/devil.gif'>");
-      text = Tools.subString(text, ">D",  "<img width=15 height=15 src='file:../base/gui/chat/evilgrin.gif'>");
-      text = Tools.subString(text, ">(",  "<img width=16 height=16 src='file:../base/gui/chat/madfire.gif'>");
-
-      text = Tools.subString(text, ";P",  "<img width=15 height=15 src='file:../base/gui/chat/flirt.gif'>");
-      text = Tools.subString(text, "8D",  "<img width=15 height=15 src='file:../base/gui/chat/horny.gif'>");
-      text = Tools.subString(text, ">#",  "<img width=15 height=15 src='file:../base/gui/chat/nono.gif'>");
-      text = Tools.subString(text, "|O",  "<img width=15 height=15 src='file:../base/gui/chat/yawn.gif'>");
-
-    //try {
-      if (DataManager.SHOW_DEBUG)
-        System.out.println("insertString");
-      //doc_chat.insertString (doc_chat.getLength(), text+"<br>\n", attribut );
-
-      strBuffer += text + "<br>\n";
-
-      Runnable runnable = new Runnable() {
-        public void run() {
-          messagesPane.setText(strBuffer);
-          messagesPane.repaint();
-        }
-      };
-      SwingUtilities.invokeLater( runnable );
-
-    /*} catch(BadLocationException e) {
-      e.printStackTrace();
-      return;
-    }*/
-
-    // TRICK TRICK TRICK TRICK TRICK
-    //System.out.println( "LENGTH str:"+strBuffer.length()+" mTxt:"+messagesPane.getText().length());
-    // we want the scrollbars to move when some text is added...
-    //if (isShowing())
-    //  messagesPane.setCaretPosition( strBuffer.length() );
-    // TRICK TRICK TRICK TRICK TRICK
+  
+  public void appendText(String s) {
+    chatDisplay.appendText(s);
   }
-
- /*------------------------------------------------------------------------------------*/
-
 }
 
   

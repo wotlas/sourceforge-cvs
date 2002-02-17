cc32b8ad7a8e6db0845c740b28059ff3f90e7e8f
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2002-02-17 08:30:55 +0000

    new wizard messages

---
+++ wotlas/src/wotlas/common/message/account/AccountCreationEndedMessage.java
@@ -0,0 +1,96 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+ 
+package wotlas.common.message.account;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import wotlas.libs.net.NetMessage;
+
+
+/** 
+ * To notice the client that the account creation has ended ()successfully).
+ * (Message Sent by Server)
+ *
+ * @author Aldiss
+ */
+
+public class AccountCreationEndedMessage extends NetMessage
+{
+ /*------------------------------------------------------------------------------------*/
+
+   // client ID part
+      protected int clientID;
+
+   // server ID part
+      protected int serverID;
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor.
+   */
+     public AccountCreationEndedMessage() {
+          super();
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** Constructor with IDs.
+   *
+   * @param clientID client local ID
+   * @param serverID server ID who created the client account
+   */
+     public AccountCreationEndedMessage(int clientID, int serverID) {
+         super();
+         this.clientID = clientID;
+         this.serverID = serverID;
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we put your message data on the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param ostream data stream where to put your data (see java.io.DataOutputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void encode( DataOutputStream ostream ) throws IOException {
+            ostream.writeInt(clientID);
+            ostream.writeInt(serverID);
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+  /** This is where we retrieve our message data from the stream. You don't need
+   * to invoke this method yourself, it's done automatically.
+   *
+   * @param istream data stream where you retrieve your data (see java.io.DataInputStream)
+   * @exception IOException if the stream has been closed or is corrupted.
+   */
+     public void decode( DataInputStream istream ) throws IOException {
+            clientID = istream.readInt();
+            serverID = istream.readInt();
+     }
+
+ /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+}
+

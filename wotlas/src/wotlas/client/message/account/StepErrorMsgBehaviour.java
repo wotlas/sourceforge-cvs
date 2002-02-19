786e9cb596f9f8f78f26ad27c425ebc1b62785b9
Pierre Nguyen <petrus0@users.sourceforge.net>
2002-02-19 18:48:23 +0000

    message for JStepWizard

---
+++ wotlas/src/wotlas/client/message/account/StepErrorMsgBehaviour.java
@@ -0,0 +1,74 @@
+/*
+ * Light And Shadow. A Persistent Universe based on Robert Jordan's Wheel of Time Books.
+ * Copyright (C) 2001 - WOTLAS Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package wotlas.client.message.account;
+
+import java.io.IOException;
+
+import wotlas.client.DataManager;
+
+import wotlas.common.message.account.*;
+import wotlas.libs.net.NetMessageBehaviour;
+
+/**
+ * Associated behaviour to the StepErrorMessage...
+ *
+ * @author Petrus
+ */
+
+public class StepErrorMsgBehaviour extends StepErrorMessage implements NetMessageBehaviour
+{
+ /*------------------------------------------------------------------------------------*/
+
+   /** To tell if this message is to be invoked later or not.
+    */
+     private boolean invokeLater = true;
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Constructor.
+   */
+  public StepErrorMsgBehaviour() {
+    super();
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+  /** Associated code to this Message...
+   *
+   * @param sessionContext an object giving specific access to other objects needed to process
+   *        this message.
+   */
+  public void doBehaviour( Object sessionContext ) {
+      //DataManager dataManager = (DataManager) sessionContext;
+      DataManager dataManager = DataManager.getDefaultDataManager();
+
+       if( invokeLater ) {
+           invokeLater = false;
+           dataManager.invokeLater( this );
+           return;
+       }
+
+      dataManager.showWarningMessage(info);
+  }
+
+ /*------------------------------------------------------------------------------------*/
+
+}
+

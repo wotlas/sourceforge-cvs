98f57e3064407829882cb8af8130f22b75ea34e5
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2002-02-22 14:35:49 +0000

    corrected typo

--- wotlas/src/wotlas/server/chat/FindChatCommand.java
+++ wotlas/src/wotlas/server/chat/FindChatCommand.java
@@ -35,7 +35,7 @@ public class FindChatCommand implements ChatCommand
 
    /** Returns the first part of the chat command. For example if your chat command
     *  has the following format '/msg:playerId:message' the prefix is '/msg' ( note
-    *  that there is ':' at the end).
+    *  that there is no ':' at the end).
     *  Other example : if your command is '/who' the prefix is '/who'. 
     *
     * @return the chat command prefix that will help identify the command.

526ed2065db348514cb7bdbfa7eda2324e67c9c4
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2002-04-02 18:31:38 +0000

    updated bat commands

--- wotlas/bin/register-server.bat
+++ wotlas/bin/register-server.bat
@@ -1,3 +1,12 @@
+@echo off
+
+rem
+rem  Server Setup Start-up Options
+rem
+rem  -base [dir]  : to tell where to take the data (default is '../base').
+rem
+
+echo Starting Register Utility...
 cd ..
 cd classes
 java -classpath . wotlas.server.setup.ServerSetup
--- wotlas/bin/server.bat
+++ wotlas/bin/server.bat
@@ -1,3 +1,14 @@
+@echo off
+
+rem
+rem  Server Start-up Options
+rem
+rem  -erroronly   : to only display ERROR, CRITICAL and FAILURE messages.
+rem  -daemon      : No debug messages are displayed, logs are directly saved to disk.
+rem  -debug       : to print a lot of debug information.
+rem  -base [dir]  : to tell where to take the data (default is '../base').
+rem
+
 cd ..
 cd classes
 java -classpath . wotlas.server.ServerDirector

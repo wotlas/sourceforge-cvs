27e9add715d5e5168b61f093b08c3a050f0a211a
Bertrand Le Nistour <lenistour@users.sourceforge.net>
2002-04-20 17:17:00 +0000

    new Ant environment for wotlas

---
+++ wotlas/build/build.bat
@@ -0,0 +1,115 @@
+@echo off
+
+set ENVTEST=This just checks whether there is additional environment space.
+if "%ENVTEST%"=="" goto increase_env
+set ENVTEST=
+
+rem ------------------------------------------------------------------------------
+rem EDIT FOR YOUR CONFIG :
+
+rem Enter your java JDK directory and remove the 'rem'
+set JAVA_HOME=c:\progra~1\javasoft\j2se1.4
+
+rem Enter the path to jikes if you want to use it instead of javac
+rem set JIKESPATH=../bin/win32/
+
+rem ------------------------------------------------------------------------------
+
+set ANT_HOME=../
+
+if not "%OS%"=="Windows_NT" goto win9xStart
+:winNTStart
+@setlocal
+
+rem Need to check if we are using the 4NT shell...
+if "%eval[2+2]" == "4" goto setup4NT
+
+rem On NT/2K grab all arguments at once
+set ANT_CMD_LINE_ARGS=%*
+goto doneStart
+
+:setup4NT
+set ANT_CMD_LINE_ARGS=%$
+goto doneStart
+
+:win9xStart
+rem Slurp the command line arguments.  This loop allows for an unlimited number of 
+rem agruments (up to the command line limit, anyway).
+
+set ANT_CMD_LINE_ARGS=
+
+:setupArgs
+if %1a==a goto doneStart
+set ANT_CMD_LINE_ARGS=%ANT_CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+
+:doneStart
+rem This label provides a place for the argument list loop to break out 
+rem and for NT handling to skip to.
+
+if "%ANT_HOME%"=="" goto noAntHome
+goto checkJava
+
+:noAntHome
+echo ANT_HOME is not set and ant could not be located. Please set ANT_HOME.
+goto end
+
+:checkJava
+set _JAVACMD=%JAVACMD%
+set LOCALCLASSPATH=%CLASSPATH%
+for %%i in ("%ANT_HOME%\lib\*.jar") do call "%ANT_HOME%\build\lcp.bat" %%i
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java
+if exist "%JAVA_HOME%\lib\tools.jar" call "%ANT_HOME%\build\lcp.bat" %JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%\lib\classes.zip" call "%ANT_HOME%\build\lcp.bat" %JAVA_HOME%\lib\classes.zip
+goto checkJikes
+
+:noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=java
+echo.
+echo Warning: JAVA_HOME environment variable is not set.
+echo   If build fails because sun.* classes could not be found
+echo   you will need to set the JAVA_HOME environment variable
+echo   to the installation directory of java.
+echo.
+
+:checkJikes
+if not "%JIKESPATH%" == "" goto runAntWithJikes
+
+:runAnt
+"%_JAVACMD%" -classpath "%LOCALCLASSPATH%" -Dant.home="%ANT_HOME%" %ANT_OPTS% org.apache.tools.ant.Main %ANT_CMD_LINE_ARGS%
+goto end
+
+:runAntWithJikes
+echo Using Jikes to compile...
+"%_JAVACMD%" -classpath "%LOCALCLASSPATH%" -Dant.home="%ANT_HOME%" -Djikes.class.path="%JIKESPATH%" %ANT_OPTS% org.apache.tools.ant.Main %ANT_CMD_LINE_ARGS%
+goto end
+
+:increase_env
+echo Increasing environment space
+if not exist %comspec% goto nocomspec
+
+:comspec
+rem %comspec% points to an existing command interpreter
+%comspec% /E:4096 /C build.bat %1 %2 %3 %4 %5
+goto end
+
+:nocomspec
+rem %comspec% is not set, trying command.com
+command /E:4096 /C build.bat %1 %2 %3 %4 %5
+goto end
+
+:end
+set LOCALCLASSPATH=
+set _JAVACMD=
+set ANT_CMD_LINE_ARGS=
+
+if not "%OS%"=="Windows_NT" goto mainEnd
+:winNTend
+@endlocal
+
+:mainEnd
+if exist "%HOME%\antrc_post.bat" call "%HOME%\antrc_post.bat"
+
--- wotlas/build/compile-readme.txt
+++ wotlas/build/compile-readme.txt
@@ -1,6 +1,19 @@
-#############################################
-### How do I compile the wotlas project ? ###
-#############################################
+#
+# How do I compile the wotlas project ?
+#
+
+Well, You have 2 solutions :
+
+
+SOLUTION 1 - ANT
+----------------
+
+
+
+SOLUTION 2 - GNU MAKE
+---------------------
+
+Note that this solution is no longuer supported... Our makefile has not been updated since wotlas v1.2.3. If you still want to use Gnu Make here are the steps  to follow :
 
 1 - Make sure you have the jdk1.3 (or higher version) already installed. You don't need to edit your CLASSPATH to compile wotlas.
 

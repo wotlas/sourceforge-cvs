aa54b9d01cb0192ea2721af6ffbc76ccf6653f51
Julien Villers <elann@users.sourceforge.net>
2002-05-19 11:01:13 +0000

    Parametric version.

--- wotlas/src/test/elann/jdoc.bat
+++ wotlas/src/test/elann/jdoc.bat
@@ -1,1 +1,31 @@
-javadoc -d javadoc -author -nodeprecated -windowtitle "Object Management System" src\*.java src\armors\*.java src\containers\*.java src\interfaces\*.java src\magicals\*.java src\usefuls\*.java src\valueds\*.java src\weapons\*.java
+@echo off
+
+rem The visibility level.
+rem May be public, private, protected, package
+rem ==========================================
+set visib=package
+
+rem Add verbose to show more
+rem Add quiet to show less
+rem ======================
+set verbose=
+
+rem The main title
+rem ==============
+set windowtitle="Object Management System"
+
+rem The destination directory
+rem =========================
+set dir=javadoc
+
+rem The source files
+rem No line breaks
+rem ==============
+set src=objects\*.java objects\armors\*.java objects\containers\*.java objects\interfaces\*.java objects\inventories\*.java objects\magicals\*.java objects\usefuls\*.java objects\valueds\*.java objects\weapons\*.java
+
+
+rem The command line
+rem ================
+javadoc %verbose% -d %dir% -author -nodeprecated -%visib% -windowtitle %windowtitle% %src%
+
+echo on

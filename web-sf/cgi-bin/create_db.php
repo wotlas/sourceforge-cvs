9c1ee17e5030a0bd1821e3fcc12f126603938df2
Julien Villers <elann@users.sourceforge.net>
2002-07-17 12:21:44 +0000

    Moving php files into cgi-bin

---
+++ web-sf/cgi-bin/create_db.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+<title>Wotlas - MySQL Tables creation</title>
+</head>
+
+<body>
+
+<?
+	if ($pass!="Bob") // the Creator ;-)
+		die("No op !</body></html>");
+		
+	include("connect.inc");
+	
+	$connect=Connect();
+	
+	$query="DROP TABLE news";
+	mysql_query($query,$connect);
+
+	$query="CREATE TABLE news (num INT UNSIGNED NOT NULL AUTO_INCREMENT,date DATE,titre VARCHAR(128) NOT NULL,contenu BLOB, PRIMARY KEY(num))";
+	mysql_query($query,$connect) or die("<br>Echec de la requête : ".$query);
+	
+	echo "Table news créée<br>";
+	
+	mysql_close($connect);
+?>
+
+
+</body>
+
+</html>

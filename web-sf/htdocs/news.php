8069b738822bf267fe6f0b3f88bb56e1925d2b56
Julien Villers <elann@users.sourceforge.net>
2002-07-22 11:30:25 +0000

    Total news' number counted to improve historical browsing.

--- web-sf/htdocs/news.php
+++ web-sf/htdocs/news.php
@@ -36,12 +36,26 @@ Click <a href="overview.html">here</a> for an overview of the Wotlas project.
 	if ($end=="" || $end<=$start)
 		$end=$start+9;
 		
-	$query="SELECT * FROM news ORDER BY DATE DESC,NUM DESC LIMIT ".$start.",".$end;
+	$query="SELECT * FROM news ORDER BY DATE DESC,NUM DESC";
 	$result=mysql_query($query,$connect) or die("Echec de la requête : ".$query."</body></html>");
 	
-	if (mysql_num_rows($result)!=0)
+	$nb=mysql_num_rows($result);
+
+	if ($nb<$start)
 	{
-		while ($lin=mysql_fetch_array($result))
+		echo "<center>";
+		echo "<br>No news in the specified boundaries : ".$start." - ".$end." !<br><br>";		
+		echo "<a href=news.php?start=0>10 latest news</a>";
+		echo "</center>";
+		mysql_free_result($result);
+		mysql_close($connect);
+		die("</body></html>");
+	}	
+
+	for ($i=1;$i<$start;$i++)
+		mysql_fetch_array($result);	
+	
+	while (($lin=mysql_fetch_array($result)) && ($i<$end))
 		{	
 	 		echo "\n<blockquote>";
 	 		echo "\n<img src=\"images/button.gif\" width=14 height=14 align=absmiddle> <b><font size=3>";
@@ -53,27 +67,22 @@ Click <a href="overview.html">here</a> for an overview of the Wotlas project.
 	 		echo "</font></p>\n</blockquote>\n</blockquote>\n";
 	 		echo "<!-- Anti-bug --></a></p></table>";
 	 		echo "\n<hr>\n";	
+			$i++;
 		}
 	
-		echo "<center>";		
-		if ($start>10)
-			echo "<a href=news.php?start=".($start-10).">10 next news</a> - <a href=news.php?start=".($start+10).">10 previous news</a>";
-		else
-			echo "<a href=news.php?start=0>10 last news</a> - <a href=news.php?start=10>10 previous news</a>";
-
-		echo "</center>";
-	}
-    else
+	echo "<center>";		
+	if ($nb>10)
 	{
-		echo "<center>";
-		echo "<br>No news in the specified boundaries : ".$start." - ".$end." !<br><br>";		
-		echo "<a href=news.php?start=0>10 latest news</a>";
-		echo "</center>";
-	}	
+	 if ($start>10)
+	 	echo "<a href=news.php?start=".($start-10).">10 next news</a> - <a href=news.php?start=".($start+10).">10 previous news</a>";
+	 else
+		echo "<a href=news.php?start=0>10 last news</a> - <a href=news.php?start=10>10 previous news</a>";
+	}		
+	echo "</center>";
 	
 	mysql_free_result($result);
 	mysql_close($connect);
-
 ?>
+
 </body>
 </html>
